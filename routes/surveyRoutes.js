3ddff1b816235e2f56f18060f00748eb36518c17
toygar05 <judastom@gmail.com>
2018-01-28 09:35:02 +0800

    fixes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,17 +3,19 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 
-const Survery = mongoose.model("surveys");
+const Survey = mongoose.model("survey");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin(), requireCredits(), (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = reg.body;
 
-    const survey = new survey({
+    const survey = new Survey({
       title,
       subject,
       body,
-      recipients
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
     });
   });
 };
