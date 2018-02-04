707b52cf4a9b690b63711a05902a9d82bcad7b9c
toygar05 <judastom@gmail.com>
2018-02-04 15:16:35 +0800

    form redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Header from './Header';
-import Landing from './Landing';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const Dashboard = () => <h2>Dashboard</h2>;
 const SurverNew = () => <h2>SurverNew</h2>;
 
 class App extends React.Component {
-	componentDidMount() {
-		this.props.fetchUser();
-	}
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-	render() {
-		// Container for materiul UI requires
-		return (
-			<div className="container">
-				<BrowserRouter>
-					<div>
-						<Header />
-						<Route exact path="/" component={Landing} />
-						<Route exact path="/surveys" component={Dashboard} />
-						<Route path="/surveys/new" component={SurverNew} />
-					</div>
-				</BrowserRouter>
-			</div>
-		);
-	}
+  render() {
+    // Container for materiul UI requires
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurverNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default connect(null, actions)(App);
